04771169d4e0cfa763f5d6d86446c1b186c3aa68
Abbas moradi <en.moradi.66@gmail.com>
2023-07-19 10:36:26 +0330

    add form class in forms

--- OnlineShop/accounts/views.py
+++ OnlineShop/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class UserregisterView(View):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
