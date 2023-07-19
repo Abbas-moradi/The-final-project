fca88cccea0521be764b5d35ce7ce468679b291e
Abbas moradi <en.moradi.66@gmail.com>
2023-07-19 18:19:30 +0330

    add a user register code in view

--- OnlineShop/accounts/views.py
+++ OnlineShop/accounts/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views import View
+from .forms import UserRegisterationForm
 
 
-class UserregisterView(View):
-    def get(self):
-        pass
+class UserRegisterView(View):
+    form_class = UserRegisterationForm
+
+    def get(self, request):
+        form = self.form_class
+        return render(request, 'accounts/register.html', {'form': form})
 
     def post(self):
         pass
