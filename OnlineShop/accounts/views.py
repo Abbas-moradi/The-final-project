464b5bac3e78bcbffa526d342f43a5bb728f9eb2
Abbas moradi <en.moradi.66@gmail.com>
2023-07-19 19:46:13 +0330

    otp code post method added

--- OnlineShop/accounts/views.py
+++ OnlineShop/accounts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import UserRegisterationForm
+from .forms import UserRegisterationForm, VerifyCodeForm
 import random
 from utils import send_otp_code
-from .models import OtpCode
+from .models import OtpCode, User
 from django.contrib import messages
+
 
 class UserRegisterView(View):
     form_class = UserRegisterationForm
@@ -31,8 +32,31 @@ class UserRegisterView(View):
 
 
 class UserRegisterVerifyCodeView(View):
+    form_class = VerifyCodeForm
+
     def get(self, request):
-        pass
+        form = self.form_class
+        return render(request, 'accounts/verify.html', {'form':form})
 
     def post(self, request):
-        pass
+        user_session = request.session['user_registration_info']
+        otp_code_instance = OtpCode.objects.get(phone_number=user_session['phone_number'])
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            if cd['code'] == otp_code_instance.code:
+                User.objects.create_user(
+                    user_session['phone_number'],
+                    user_session['email'],
+                    user_session['full_name'],
+                    user_session['password']
+                )
+
+                otp_code_instance.delete()
+
+                messages.success(request, 'register success', 'success')
+                return redirect('home:home')
+            else:
+                messages.error(request, 'this code is wrong', 'danger')
+                return redirect('accounts:verify_code')
+        return redirect('home:home')
