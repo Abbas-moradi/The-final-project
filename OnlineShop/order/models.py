040ea1e72056a4224d705630af62ea501f0a0e46
Abbas moradi <en.moradi.66@gmail.com>
2023-07-20 00:03:58 +0330

    add order models

--- OnlineShop/order/models.py
+++ OnlineShop/order/models.py
@@ -8,10 +8,25 @@ class Order(models.Model):
     total_amount = models.IntegerField()
     status = models.BooleanField(default=True)
 
+    class Meta:
+        ordering = ('order_date',)
+        verbose_name = 'order'
+        verbose_name_plural = 'orders'
+
+    def __str__(self) -> str:
+        return f'{self.user} - {self.order_date}'
+
 
 class OrderItems(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
     item_total_amount = models.IntegerField()
-    
+
+    class Meta:
+        ordering = ('order',)
+        verbose_name = 'order item'
+        verbose_name_plural = 'order items'
+
+    def __str__(self) -> str:
+        return f'{self.order} - {self.product}'
