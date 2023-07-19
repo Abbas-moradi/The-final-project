74653fccd7ba66291589d4d411964c7b8548bc8f
Abbas moradi <en.moradi.66@gmail.com>
2023-07-19 23:59:21 +0330

    add order model

--- OnlineShop/order/models.py
+++ OnlineShop/order/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from accounts.models import User
+from product.models import Product
 
-# Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_date = models.DateField(auto_now=True)
+    total_amount = models.IntegerField()
+    status = models.BooleanField(default=True)
+
+
+class OrderItems(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+    item_total_amount = models.IntegerField()
+    
