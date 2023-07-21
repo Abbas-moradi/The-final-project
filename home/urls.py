59ec9d7207ad5e59e88116aebc3348e44f1b7014
Abbas moradi <en.moradi.66@gmail.com>
2023-07-21 20:54:04 +0330

    complete the load and run template

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('contact/', views.Contact.as_view(), name='contact'),
+    path('shop/', views.Shop.as_view(), name='shop'),
 ]
