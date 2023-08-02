56fbe6e3c0deb0300c54cf887faf8ebf1f85f334
Abbas moradi <en.moradi.66@gmail.com>
2023-08-03 00:52:39 +0330

    create docker file and write test code

--- order/tests.py
+++ order/tests.py
@@ -1,3 +1,60 @@
 from django.test import TestCase
+from django.test import TestCase
+from django.utils import timezone
+from accounts.models import User
+from product.models import Product
+from .models import Order, OrderItems
+from django.test import TestCase
+from django.urls import reverse, resolve
+from . import views
 
-# Create your tests here.
+class TestOrderUrls(TestCase):
+
+    def test_cart_url_resolves(self):
+        url = reverse('order:shopping-cart')
+        self.assertEqual(resolve(url).func.view_class, views.CartView)
+
+    def test_cart_add_url_resolves(self):
+        product_id = 1
+        url = reverse('order:cart_add', args=[product_id])
+        self.assertEqual(resolve(url).func.view_class, views.CartAddView)
+
+    def test_cart_del_url_resolves(self):
+        product_id = 1
+        url = reverse('order:cart_del', args=[product_id])
+        self.assertEqual(resolve(url).func.view_class, views.CartDelView)
+
+    def test_checkout_url_resolves(self):
+        url = reverse('order:checkout')
+        self.assertEqual(resolve(url).func.view_class, views.Checkout)
+
+
+class OrderModelTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(full_name='test_user')
+        self.product = Product.objects.create(name='Test Product', price=10)
+
+    def test_order_creation(self):
+        order_date = timezone.now().date()
+        total_amount = 100
+        status = True
+        order = Order.objects.create(user=self.user, order_date=order_date, total_amount=total_amount, status=status)
+        
+        self.assertIsInstance(order, Order)
+        self.assertEqual(order.user, self.user)
+        self.assertEqual(order.order_date, order_date)
+        self.assertEqual(order.total_amount, total_amount)
+        self.assertEqual(order.status, status)
+
+    def test_order_items_creation(self):
+        order = Order.objects.create(user=self.user, order_date=timezone.now().date(), total_amount=100, status=True)
+        quantity = 2
+        item_total_amount = self.product.price * quantity
+        order_item = OrderItems.objects.create(order=order, product=self.product, quantity=quantity, item_total_amount=item_total_amount)
+        
+        self.assertIsInstance(order_item, OrderItems)
+        self.assertEqual(order_item.order, order)
+        self.assertEqual(order_item.product, self.product)
+        self.assertEqual(order_item.quantity, quantity)
+        self.assertEqual(order_item.item_total_amount, item_total_amount)
--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
+from .views import Products
+from .models import Product, Category
 
-# Create your tests here.
+class TestProductsView(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.category = Category.objects.create(name='Test Category', slug='test-category')
+        self.product = Product.objects.create(name='Test Product', category=self.category, available=True)
+
+    def test_get_with_category_slug(self):
+        url = reverse('shop')
+        request = self.factory.get(url, {'category_slug': self.category.slug})
+        response = Products.as_view()(request, category_slug=self.category.slug)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Test Product', response.content)
+
+    def test_get_without_category_slug(self):
+        url = reverse('shop')
+        request = self.factory.get(url)
+        response = Products.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Test Product', response.content)
+
+    def test_get_with_invalid_category_slug(self):
+        url = reverse('shop')
+        invalid_slug = 'invalid-slug'
+        request = self.factory.get(url, {'category_slug': invalid_slug})
+        response = Products.as_view()(request, category_slug=invalid_slug)
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn(b'Test Product', response.content)
+
